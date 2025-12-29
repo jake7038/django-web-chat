5fd1892a1370a3d638f0ca5bcd23a965cbbb6647
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2025-12-28 23:12:07 -0300

    Feat: Post django Admin

--- webchat/blog/models/post.py
+++ webchat/blog/models/post.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 STATUS = (
     (0, 'Draft'),
-    (1, 'pUBLISH')
+    (1, 'Publish')
 )
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
